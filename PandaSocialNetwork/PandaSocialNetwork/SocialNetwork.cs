c2041c779d9a5897e5541278ad18ace5167f0e6d
rokn <amindov@abv.bg>
2016-01-21 20:34:09 +0200

    Implemented connections level methods

--- PandaSocialNetwork/PandaSocialNetwork/SocialNetwork.cs
+++ PandaSocialNetwork/PandaSocialNetwork/SocialNetwork.cs
@@ -1,10 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using PandaSocialNetworkInterfaces;
 
-namespace PandaSocialNetwork {
-    class SocialNetwork {
-    }
+namespace PandaSocialNetwork
+{
+    class SocialNetwork : ISocialNetwork
+    {
+	    private Dictionary<string, IPanda> _pandaUsers;
+
+	    public SocialNetwork()
+	    {
+		    _pandaUsers = new Dictionary<string, IPanda>();
+		}
+
+	    public void AddPanda(IPanda panda)
+	    {
+		    throw new System.NotImplementedException();
+	    }
+
+	    public bool HasPanda(IPanda panda)
+	    {
+		    throw new System.NotImplementedException();
+	    }
+
+	    public void MakeFriends(IPanda panda1, IPanda panda2)
+	    {
+		    throw new System.NotImplementedException();
+	    }
+
+	    public bool AreFriends(IPanda panda1, IPanda panda2)
+	    {
+		    throw new System.NotImplementedException();
+	    }
+
+	    public List<IPanda> FriendsOf(IPanda panda)
+	    {
+		    throw new System.NotImplementedException();
+	    }
+
+	    public int ConnectionLevel(IPanda panda1, IPanda panda2)
+	    {
+		    if (panda1 == panda2)
+		    {
+			    return 0;
+		    }
+
+		    var pending = new Queue<IPanda>();
+			var visited = new List<IPanda>();
+
+		    foreach (var currPanda in panda1.Friends)
+		    {
+			    var currConnection = ConnectionLevel(currPanda, panda2);
+
+			    if (currConnection >= 0)
+			    {
+				    return currConnection + 1;
+			    }
+		    }
+
+		    return -1;
+	    }
+
+	    public bool AreConnected(IPanda panda1, IPanda panda2)
+	    {
+		    return ConnectionLevel(panda1, panda2) > 0;
+	    }
+
+	    public int HowManyGenderInNetwork(int level, IPanda panda, GenderType gender)
+	    {
+		    return -1;
+	    }
+	}
 }
--- PandaSocialNetwork/PandaSocialNetworkInterfaces/IPanda.cs
+++ PandaSocialNetwork/PandaSocialNetworkInterfaces/IPanda.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PandaSocialNetworkInterfaces {
     public interface IPanda {
@@ -11,6 +7,7 @@ namespace PandaSocialNetworkInterfaces {
         GenderType Gender { get; }
         bool IsMale { get; }
         bool IsFemale { get; }
+		List<IPanda> Friends { get; } 
 
         string ToString();
         int GetHashCode();
